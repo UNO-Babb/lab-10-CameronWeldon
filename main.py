4175059525705397a2f71620acaa43691610636c
CameronWeldon <cameronweldon@unomaha.edu>
2025-05-16 20:37:43 +0000

    Done with Lab 10.

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 #MapPlot.py
-#Name:
+#Name: Cameron Weldon
 #Date:
-#Assignment:
+#Assignment: Lab 10
+
+import coffee
+import pandas
+import matplotlib.pyplot as plt
+
+#I made a graph of Coffee Beans showing the Year and Sweetness score.
+
+coffee = coffee.get_coffee()
+
+print(coffee[0]["Data"]["Scores"]["Sweetness"])
+
+years = []
+scores = []
+for bean in coffee:
+    year = bean["Year"]
+    score = bean["Data"]["Scores"]["Sweetness"]
+    years.append(year)
+    scores.append(score)
+    
+
+df = pandas.DataFrame({"Year": years,
+                        "Score": scores})
+
+print(df)
+df.plot(kind = 'scatter', x = 'Year', y = 'Score')
+
+plt.savefig("output")
